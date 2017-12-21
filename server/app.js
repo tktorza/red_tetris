b4288f12d81f3ba0655a95d2a519691437d9ba17
Louis DAUBIE <ldaubie@e3r6p17.42.fr>
2017-12-21 17:49:35 +0100

    debut socket

--- server/app.js
+++ server/app.js
@@ -10,5 +10,12 @@ app.use(express.static('public'));
 
 
 
+io.on('connection', (socket) => {
+	console.log("la")
+	console.log(socket.id)
+	socket.on('test', (la) => {
+		socket.emit('ok', la)
+	})
+})
 
 server.listen(9000)
